7787bb970b0e353a1258085dc6550889af4a96d8
eyosi168 <eyosisol123@gmail.com>
2026-03-01 23:19:25 +0300

    suspense with react router dom

--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -1,14 +1,12 @@
-import apiRequest from "./apiRequest.js"
-import {defer} from "react-router-dom"
-export const singlePageLoad = async({request,params})=>{
-const res = await apiRequest("/posts/"+params.id)
- return res.data
-
-}
-export const listPageLoad = async({request,params})=>{
-   const query = request.url.split("?")[1]
-   const postPromise = await apiRequest("/posts?"+query)
-   return defer({
+import apiRequest from "./apiRequest.js";
+export const singlePageLoad = async ({ request, params }) => {
+  const res = await apiRequest("/posts/" + params.id);
+  return res.data;
+};
+export const listPageLoad = async ({ request, params }) => {
+  const query = request.url.split("?")[1];
+  const postPromise = apiRequest("/posts?" + query);
+  return{
     postResponse: postPromise,
-   })
-    }
+  };
+};
