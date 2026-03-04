a3be566f55bc15960ff4c4f9e1eec6ef3d8987d5
eyosi168 <eyosisol123@gmail.com>
2026-03-04 16:51:11 +0300

    models for chat and message finished

--- server/app.js
+++ server/app.js
@@ -4,7 +4,9 @@ import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
-import userRoute from "./routes/user.route.js"
+import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 
  const app = express();
  app.use(express.json())
@@ -15,6 +17,8 @@ import userRoute from "./routes/user.route.js"
  app.use("/api/users",userRoute)
  app.use("/api/posts",postRoute)
  app.use("/api/test",testRoute)
+ app.use("/api/chats",chatRoute)
+ app.use("/api/messages",messageRoute)
 
  
  app.listen(8800,()=>{
---
+++ server/controller/chat.controller.js
@@ -0,0 +1,43 @@
+import prisma from "../lib/prisma.js"
+
+
+export const getChats = async (req,res)=>{
+    
+    try{
+        
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({mesage:"Failed to get chats"})
+    }
+}
+export const getChat = async (req,res)=>{
+    
+    try{
+        
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({mesage:"Failed to get chat"})
+    }
+}
+export const addChat = async (req,res)=>{
+    
+    try{
+        
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({mesage:"Failed to add chats"})
+    }
+}
+export const readChat = async (req,res)=>{
+    
+    try{
+        
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({mesage:"Failed to read chats"})
+    }
+}
